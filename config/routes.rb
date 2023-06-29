f2838154cde61bb909f796fe25b10ad8c6a98ea9
torcuata22 <marilynjmarquez@gmail.com>
2023-06-29 11:48:43 -0400

    three

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  get "/articles/new", to: "articles#new", as: :new_article
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show", as: :article
+
+ 
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "articles#index"
+  
 end
