3da8f90883a8867d079a70afd524a28f2b4d2579
torcuata22 <marilynjmarquez@gmail.com>
2023-06-30 14:20:57 -0400

    just in case

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,32 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params) 
+            redirect_to articles_path 
+        else 
+            render :edit 
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
+  put "/articles/:id", to: "articles#update"
+  get "/articles/:id/edit", to: "articles#edit"
+  post "/articles", to: "articles#create"
   get "/articles/new", to: "articles#new", as: :new_article
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show", as: :article
